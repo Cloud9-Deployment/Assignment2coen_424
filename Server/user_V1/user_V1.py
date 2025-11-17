6bf62a2cdde6fd493cbfac9c94684c573c9ba56f
JonaBaron <133152403+JonaBaron@users.noreply.github.com>
2025-11-17 15:47:17 -0500

    Add requirements files and Dockerfiles for event and user services; update user V1 service with MongoDB connection and user count functionality

--- Server/user_V1/user_V1.py
+++ Server/user_V1/user_V1.py
@@ -1,14 +1,26 @@
+from urllib.parse import quote_plus
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+import certifi
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
+# MongoDB Connection ------------------------------
+
 try:
-    mongo_uri = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/')
-    client = MongoClient(mongo_uri)
+    username = quote_plus(os.getenv('MONGODB_USER'))
+    password = quote_plus(os.getenv('MONGODB_PASSWORD'))
+    mongo_uri = f"mongodb+srv://{username}:{password}@cluster0.4agn1ar.mongodb.net/?appName=Cluster0"
+    
+    # Add tlsCAFile parameter with certifi's CA bundle
+    client = MongoClient(
+        mongo_uri,
+        tlsCAFile=certifi.where()
+    )
     db = client[os.getenv('MONGODB_USER_DB', 'user_database')]
     users_collection = db['users']
     print("✓ Connected to MongoDB - User Database (V1)")
@@ -20,7 +32,8 @@ except Exception as e:
 # To greet
 @app.route('/', methods=['GET'])
 def hello_world():
-    return 'User V1 Service is running!'
+    results= "User V1 Service is running! With " + str(get_number_of_users()) + " users."
+    return results
 
 # To create a user
 @app.route('/user', methods=['POST'])
@@ -43,9 +56,24 @@ def update_user(user_id):
 
 # Helper functions --------------------------------
 
+# Function to get number of users
+def get_all_users():
+    print("Fetching all users from User database:")
+    result = list(users_collection.find())
+    return result
+
+def get_number_of_users():
+    users = get_all_users()
+    print("Number of users in User V1 service:", len(users))
+    return len(users)
+
 # Helper function to simulate user creation
 def userCreation(email, address):
-    print("Received user data at User V1 service:", email, address)
+    results = users_collection.insert_one({
+        "email": email,
+        "delivery_address": address
+    })
+    print("Inserted user ", email, " ", address, " with id:", results.inserted_id)
 
 # Helper function to simulate user update
 def userUpdate(user_id, email, address):
@@ -54,3 +82,4 @@ def userUpdate(user_id, email, address):
 if __name__ == '__main__':
     print("Microservices user V1 !!!!")
     app.run(host='0.0.0.0', port=5000, debug=True)
+    
